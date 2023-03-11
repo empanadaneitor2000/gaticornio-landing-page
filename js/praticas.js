9406d37312046e463bc5dfb24d5ffa4a6d936478
empanadaneitor2000 <andresfelipemj123@gmail.com>
2023-03-11 18:00:43 -0500

    firtspush

---
+++ js/praticas.js
@@ -0,0 +1,58 @@
+/* 
+
+// ehercicio 1
+var np1 = parseInt (prompt('dame un  numero' ));
+
+var np2 = parseInt (prompt('dame otro numero' ));
+
+if ( np1 > np2) {
+    alert("este es el mayor" + np1);
+    alert("este el menor" + np2)
+} else  if (np1 < np2) {
+        alert("este es el mayor" + np2);
+        alert("este el menor" + np1)
+} else if (np1 == np2) {
+    alert("son iguales boluda");
+}  
+*/
+
+// ejercicio 2
+/*
+var a = 50;
+while (a>30) {
+    a = a - 7 
+} console.log(a);
+
+a = new[7];
+{10,5,23,7,15,65,85};
+b = 0;
+while (a.length < 7) {
+    b = b +a.item;
+}
+console.log
+*/
+
+//ejercicios de rray y funciones
+const button = document.querySelector("button");
+function action() {
+    alert("Hello!");
+  };
+  button.addEventListener('click', action);
+
+function cambiartext(){
+
+}
+
+function capturador(){
+    
+    var t1 = document.getElementById("box-text");
+   
+       /* if(t1 != null||" "){
+            window.alert("escribe algo marikon");
+        }else{
+            window.alert("bien hecho marikon");
+        }*/
+   
+   // el.addEventListener("click", cambiartext, false);
+    }
+    
